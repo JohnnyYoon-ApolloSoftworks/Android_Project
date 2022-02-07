0c42072f2bec5539338e2762940f88244fb10749
JohnnyOnTheMoon <apollo0611@icloud.com>
2022-02-07 17:01:17 +0900

    Add [BtnPrint]

--- TEST/app/src/main/java/com/apolloelectronics/test/MainActivity.kt
+++ TEST/app/src/main/java/com/apolloelectronics/test/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.apolloelectronics.test
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.apolloelectronics.test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnSay.setOnClickListener {
+            binding.textView.text = "Hello Kotlin!"
+        }
     }
 }
